49395131d3c3a446862e3e8d873edb9a093cbdf1
William Duckworth <william@wduckworth.me.uk>
2018-04-01 19:12:26 +0100

    Xcode Project
    I created the Xcode project and added the mapkit view to the main view on the storyboard

---
+++ Benchmarks/Benchmarks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Benchmarks
+//
+//  Created by William Duckworth on 01/04/2018.
+//  Copyright © 2018 William Duckworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
