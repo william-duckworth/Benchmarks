35575e6e7eff4d6345d3d5be723b5df89ea4af0f
William Duckworth <william@wduckworth.me.uk>
2018-04-02 01:17:55 +0100

    Parsing Text File
    I added the code to parse the text file - at the moment it gets an index out of range error

--- Benchmarks/Benchmarks/ViewController.swift
+++ Benchmarks/Benchmarks/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     let annotation = MKPointAnnotation()
     let annotation1 = MKPointAnnotation()
     
+    var benchmarkDictionary = [String:String]()
+    var benchmarkArray = NSMutableArray()
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
@@ -31,6 +34,38 @@ class ViewController: UIViewController {
         mapView.addAnnotation(annotation)
         annotation1.coordinate = CLLocationCoordinate2D(latitude: 51.697451, longitude: -2.218123)
         mapView.addAnnotation(annotation1)
+        
+        let path = Bundle.main.path(forResource: "Benchmarks", ofType: "txt")
+        let fileManager = FileManager.default
+        
+        if fileManager.fileExists(atPath: path!) {
+            do {
+                let fullText = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+                var readings = fullText.components(separatedBy: "\n") as [String]
+                for i in 4..<readings.count {
+                    let benchmarkData = readings[i].components(separatedBy: "\t")
+                    
+                    benchmarkDictionary["NG_LETTERS"] = "\(benchmarkData[0])"
+                    benchmarkDictionary["EASTING"] = "\(benchmarkData[1])"
+                    benchmarkDictionary["NORTHING"] = "\(benchmarkData[2])"
+                    benchmarkDictionary["HEIGHT"] = "\(benchmarkData[3])"
+                    benchmarkDictionary["DATUM"] = "\(benchmarkData[4])"
+                    benchmarkDictionary["NETWORK"] = "\(benchmarkData[5])"
+                    benchmarkDictionary["ORDER"] = "\(benchmarkData[6])"
+                    benchmarkDictionary["TYPE_OF_MARK"] = "\(benchmarkData[7])"
+                    benchmarkDictionary["HEIGHT_ABOVE_GROUND"] = "\(benchmarkData[8])"
+                    benchmarkDictionary["VERIFIED_DATE"] = "\(benchmarkData[9])"
+                    benchmarkDictionary["LEVELING DATE"] = "\(benchmarkData[10])"
+                    benchmarkDictionary["DESCRIPTION"] = "\(benchmarkData[11])"
+                    
+                    benchmarkArray.add(benchmarkDictionary)
+                }
+            } catch let error as NSError {
+                print("Error: \(error)")
+            }
+        }
+        
+        print(benchmarkArray.count)
     }
 
     override func didReceiveMemoryWarning() {
